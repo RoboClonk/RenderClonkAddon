9b25e24bd3af290e6153901e1933e9f7a6683a13
Robin Hohnsbeen <robin@hohnsbeen.de>
2022-07-02 12:31:57 +0200

    Set correct version number

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ bl_info = {
 	"author" : "Robin Hohnsbeen",
 	"description" : "For importing Clonk meshes and rendering spritesheets.",
 	"blender" : (3, 0, 1),
-	"version" : (2, 2, 1),
+	"version" : (2, 3, 0),
 	"location" : "",
 	"warning" : "",
 	"category" : "Render"
@@ -59,7 +59,7 @@ importlib.reload(MetaData)
 importlib.reload(IniPort)
 
 
-print ("Render Clonk 2.2")
+print ("Render Clonk 2.3")
 
 
 AddonDirectory = ""
